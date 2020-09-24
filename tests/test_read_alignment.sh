e57637162dc086d12077b35cff3cd3a305f258b3
Jacqui Keane <drjkeane@gmail.com>
2020-09-24 21:19:42 +0100

    Update module test scripts

--- tests/test_read_alignment.sh
+++ tests/test_read_alignment.sh
@@ -1,8 +1,9 @@
 cd read_alignment
+cd data
 cd ref
 zless GRCm38.68.dna.toplevel.chr7.fa.gz | head
 cd ../Exercise1/fastq/
-bwa index ../../ref/index GRCm38.68.dna.toplevel.chr7.fa.gz
+bwa index ../../ref/GRCm38.68.dna.toplevel.chr7.fa.gz
 bwa mem ../../ref/GRCm38.68.dna.toplevel.chr7.fa.gz md5638a_7_87000000_R1.fastq.gz md5638a_7_87000000_R2.fastq.gz > md5638.sam
 samtools view -O BAM -o md5638.bam md5638.sam 
 samtools sort -T temp -O bam -o md5638.sorted.bam md5638.bam 
@@ -14,4 +15,3 @@ less md5638.metrics.txt
 samtools index md5638.markdup.bam 
 samtools stats md5638.markdup.bam > md5638.markdup.stats
 plot-bamstats -p md5638_plot/ md5638.markdup.stats 
-history > ~/repo/alignment.sh
--- tests/test_rna_seq.sh
+++ tests/test_rna_seq.sh
@@ -6,14 +6,25 @@ ls PccAS_v3_hisat2.idx.*
 hisat2 -h
 hisat2 --max-intronlen 10000 -x PccAS_v3_hisat2.idx -1 MT1_1.fastq.gz -2 MT1_2.fastq.gz -S MT1.sam
 samtools view -b -o MT1.bam MT1.sam
-samtools sort -o MT1_sorted.bam MT1.bam 
-samtools index MT1_sorted.bam 
+samtools sort -o MT1_sorted.bam MT1.bam
+samtools index MT1_sorted.bam
 ls SBP*bam*
 ./map_SBP_samples.sh
 ls *.bam
-cat sleuth.R 
-cat hiseq_info.txt 
-Rscript sleuth.R 
+kallisto index
+kallisto quant
+kallisto index -i PccAS_v3_kallisto PccAS_v3_transcripts.fa
+kallisto quant -i PccAS_v3_kallisto -o MT1 -b 100 MT1_1.fastq.gz MT1_2.fastq.gz
+head MT1/abundance.tsv
+grep PCHAS_0100100 MT1/abundance.tsv
+awk -F"\t" '$1=="PCHAS_0100100" {print $5}' MT1/abundance.tsv
+kallisto quant -i PccAS_v3_kallisto -o MT2 -b 100 MT2_1.fastq.gz MT2_2.fastq.gz
+kallisto quant -i PccAS_v3_kallisto -o SBP1 -b 100 SBP1_1.fastq.gz SBP1_2.fastq.gz
+kallisto quant -i PccAS_v3_kallisto -o SBP2 -b 100 SBP2_1.fastq.gz SBP2_2.fastq.gz
+kallisto quant -i PccAS_v3_kallisto -o SBP3 -b 100 SBP3_1.fastq.gz SBP3_2.fastq.gz
+cat sleuth.R
+cat hiseq_info.txt
+Rscript sleuth.R
 awk -F "\t" '$4 < 0.01 && $5 > 0' kallisto.results | cut -f1,2,3,4,5 | head
 awk -F "\t" '$4 < 0.01 && $5 < 0' kallisto.results | cut -f1,2,3,4,5 | head
 awk -F "\t" '$4 < 0.01 && $5 < 0 {print $2}' kallisto.results | sort | uniq
