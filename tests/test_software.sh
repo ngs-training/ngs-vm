df721ce81785970293690260fdb64e7d1f25db62
Jacqui Keane <drjkeane@gmail.com>
2022-10-08 13:41:21 +0100

    Add tests for software

--- tests/test_software.sh
+++ tests/test_software.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
+set -x
 
 echo $PATH
 
 breakdancer-max
-
 samtools --help
 bcftools --version
 bedtools --version
 bowtie2 --version
-bwa --help
-gatk4
+bwa
+gatk
 freebayes
-igv &
-
 picard -h
-
 sniffles --version
 minimap2 --version
 dysgu
-
 hisat2 --version
 kallisto version
-sleuth.R
-
 macs2 --version
 meme -version
 tomtom -version
 bedGraphToBigWig
 fetchChromSizes
-r-ngsplot
-
 assembly-stats -v
 canu -version
 jellyfish --version
